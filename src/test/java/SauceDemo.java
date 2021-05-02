3ae2dfb2e804aa792119746b76d2aa0dcd84df51
Настя <nastya.martsuta@yandex.by>
2021-05-02 21:50:18 +0300

    Initial commit

---
+++ src/test/java/SauceDemo.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SauceDemo {
+    @Test
+    public void locators() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        driver.findElement(By.className("btn_action")).click();
+
+        driver.findElement(By.tagName(""));
+        driver.findElement(By.linkText("Twitter"));
+
+        driver.findElement(By.cssSelector("inventory_item_name"));
+
+    }
+}
